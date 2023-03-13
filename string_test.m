c66a6600abad1386c2830616be813a6a0d247576
Paul Mackay <pmichaelmackay@protonmail.com>
2023-03-13 16:42:37 -0700

    working on parsing xlsx file

---
+++ string_test.m
@@ -0,0 +1,17 @@
+load testpaul1.mat
+load mdcodes1.mat
+
+all_list = floats wires chains
+
+count = 0
+nomatch = 0
+for i = 1:rows(c)-1
+    for j = 1:rows(chains)
+        if (startsWith(erase(chains(j,:),'"'), erase(c(i,1),'"'), "IgnoreCase", true) == 1) 
+            count = count+1;
+            printf("COUNT SO FAR: %d \nMatch", count)
+            disp(c(i,1))
+
+        endif
+    endfor
+endfor
