995509cac8da41aa5e932682aa5311ac1d3df490
Paul Mackay <pmichaelmackay@protonmail.com>
2023-04-11 12:45:46 -0700

    gui changed and exceptions made

--- main/spreadsheet_to_mat.m
+++ main/spreadsheet_to_mat.m
@@ -27,9 +27,9 @@ function spreadsheet_to_mat
     load mdcodes.mat
     load empty_mooring.mat
     global testlen
-    
 
-    
+
+
 
     %[ifile,ipath]=uigetfile({'*.xlsx'},'Load Spread Sheet');
     %[a,b,c]=xlsread(ifile);
@@ -40,7 +40,7 @@ function spreadsheet_to_mat
     [a,b,c]=xlsread([ipath ifile]);
     save xlsdata1.mat c
     load xlsdata1.mat
-    
+
     moorname = c{1,1};
     moordepth=num2str(c{2,1});
     dep = [moordepth ' 10 0'];
@@ -69,7 +69,7 @@ function spreadsheet_to_mat
         endif
     endfor
     start=i;
-    
+
     all_list = [""];
     for m = 1:rows(floats)
         all_list(rows(all_list)+1,:)=floats(m,:);
@@ -97,105 +97,13 @@ function spreadsheet_to_mat
     match = 0;
     flag1=0;
     for i = start:rows(c)
-        match=0;
-        k=1;
-        testlen={};
-        while (k <= rows(all_list))
-            all_list = [""];
-            for m = 1:rows(floats)
-                all_list(rows(all_list)+1,:)=floats(m,:);
-            endfor
-            for m = 1:rows(wires)
-                all_list(rows(all_list)+1,:)=wires(m,:);
-            endfor
-            for m = 1:rows(chains)
-                all_list(rows(all_list)+1,:)=chains(m,:);
-            endfor
-            for m = 1:rows(acrels)
-                all_list(rows(all_list)+1,:)=acrels(m,:);
-            endfor
-            for m = 1:rows(cms)
-                all_list(rows(all_list)+1,:)=cms(m,:);
-            endfor
-            for m = 1:rows(anchors)
-                all_list(rows(all_list)+1,:)=anchors(m,:);
-            endfor
-            for m = 1:rows(miscs)
-                all_list(rows(all_list)+1,:)=miscs(m,:);
-            endfor
-            tmp = strsplit(all_list(k,1:30), "  ");
-            testlen = length(tmp{1,1});
-            %if (startsWith(all_list(k,testlen), c(i,1), "IgnoreCase", true) == 1)
-            if (startsWith(all_list(k,1:testlen), c(i,1), "IgnoreCase", true) == 1) && (testlen == length(c{i,1}))
-                %printf("%d == %d   %s == %s\n",testlen,length(c{i,1}), c{i,1}, tmp{1,1});
-                ldng = waitbar(1);
-                match=1;
-                elenum=length(B)+1;
-                insert=elenum;
-                mb=length(B);
-                bump=[insert+1:mb+1];
-                moorele(bump,:)=moorele(elenum:mb,:);
-                B(bump)=B(elenum:mb);
-                H(:,bump)=H(:,elenum:mb);
-                Cd(bump)=Cd(elenum:mb);
-                ME(bump)=ME(elenum:mb);
-
-
-                moorele(elenum,:)=all_list(k,format(1,1):format(1,2));
-                B(elenum)=str2num(all_list(k,format(2,1):format(2,2)));
-                H(1,elenum)=str2num(all_list(k,format(3,1):format(3,2)))/100; % convert to metres pm
-                H(2,elenum)=str2num(all_list(k,format(4,1):format(4,2)))/100;
-                H(3,elenum)=str2num(all_list(k,format(5,1):format(5,2)))/100;
-                H(4,elenum)=0;
-                ME(elenum)=inf;  % by default set modulus of elasticity to infinity (no stretch)
-                if H(1,elenum)==1,
-                    H(1,elenum)=c{i,4};
-                    H(4,elenum)=1; % flag for wire/chain elements, sub-divide later
-                    break;
-                    mat=str2num(all_list(k,format(7,1):format(7,2)));
-                    if mat==1, % steel
-                        ME(elenum)=1.38e11;
-                    elseif mat==2, % Nylon
-                        ME(elenum)=3.45e8;
-                    elseif mat==3, % Dacron
-                        ME(elenum)=8.0e8;
-                    elseif mat==4, % Polyprop
-                        ME(elenum)=3.45e8;
-                    elseif mat==5, % Polyethy
-                        ME(elenum)=6.9e8;
-                    elseif mat==6, % Kevlar
-                        ME(elenum)=6.9e10;
-                    elseif mat==7, % Aluminum
-                        ME(elenum)=7.6e10;
-                    elseif mat==8, % Dyneema
-                        ME(elenum)=1.0e11;
-                    end
-                else
-                    H(4,elenum)=2; % flag for shackles and koiners
-                endif
-                Cd(elenum)=str2num(all_list(k,format(6,1):format(6,2)));
-                elenum0=elenum;
-                elenum=length(B)+1;
-                insert=0;
-                k=k+1;
-            elseif (k == rows(all_list) && match == 0)
-                %printf("%d\n",)
-                %printf("list(k+1): %s\nk: %d\nc(i,1): %d\n",list(k,:),k, c{i,1})
-                printf("%d\n",match)
-                %addelement_xls2;waitfor(h_push_save);
-                addelement_xls2;
-                set(h_edit_elename,'String',c{i,1});
-                set(h_edit_elebuoy,'String',num2str(-1*c{i,3}));
-                set(h_edit_eledim,'String',num2str(100*c{i,4}));
-                add_name=get(h_edit_elename, 'String');
-                add_buoy=get(h_edit_elebuoy, 'String');
-                add_length=get(h_edit_eledim, 'String');
-                warning("Please fill in data for the following: %s", c{i,1});
-                %pause;
-                k=1;
-                match=0;
-                %waitfor(h_push_add);
-                save ('mdcodes.mat','acrels','cms','format','miscs','anchors','chains','floats','wires','all_list');
+        if startsWith(c(i,1),"estimated")
+            break;
+        else
+            match=0;
+            k=1;
+            testlen={};
+            while (k <= rows(all_list))
                 all_list = [""];
                 for m = 1:rows(floats)
                     all_list(rows(all_list)+1,:)=floats(m,:);
@@ -218,13 +126,129 @@ function spreadsheet_to_mat
                 for m = 1:rows(miscs)
                     all_list(rows(all_list)+1,:)=miscs(m,:);
                 endfor
-                waitfor(h_edit_elename);
-                %waitfor(hmaincls);figure(4);clf;close;
-            else
-                k=k+1;
-            endif
-        endwhile
+                tmp = strsplit(all_list(k,1:30), "  ");
+                testlen = length(tmp{1,1});
+                %if (startsWith(all_list(k,testlen), c(i,1), "IgnoreCase", true) == 1)
+                if (startsWith(all_list(k,1:testlen), c(i,1), "IgnoreCase", true) == 1) && (testlen == length(c{i,1}))
+                    %printf("%d == %d   %s == %s\n",testlen,length(c{i,1}), c{i,1}, tmp{1,1});
+                    ldng = waitbar(1);
+                    match=1;
+                    elenum=length(B)+1;
+                    insert=elenum;
+                    mb=length(B);
+                    bump=[insert+1:mb+1];
+                    moorele(bump,:)=moorele(elenum:mb,:);
+                    B(bump)=B(elenum:mb);
+                    H(:,bump)=H(:,elenum:mb);
+                    Cd(bump)=Cd(elenum:mb);
+                    ME(bump)=ME(elenum:mb);
+
+
+                    moorele(elenum,:)=all_list(k,format(1,1):format(1,2));
+                    B(elenum)=str2num(all_list(k,format(2,1):format(2,2)));
+                    H(1,elenum)=str2num(all_list(k,format(3,1):format(3,2)))/100; % convert to metres pm
+                    H(2,elenum)=str2num(all_list(k,format(4,1):format(4,2)))/100;
+                    H(3,elenum)=str2num(all_list(k,format(5,1):format(5,2)))/100;
+                    H(4,elenum)=0;
+                    ME(elenum)=inf;  % by default set modulus of elasticity to infinity (no stretch)
+                    if H(1,elenum)==1,
+                        H(1,elenum)=c{i,4};
+                        H(4,elenum)=1; % flag for wire/chain elements, sub-divide later
+                        %break;
+                        mat=str2num(all_list(k,format(7,1):format(7,2)));
+                        if mat==1, % steel
+                            ME(elenum)=1.38e11;
+                        elseif mat==2, % Nylon
+                            ME(elenum)=3.45e8;
+                        elseif mat==3, % Dacron
+                            ME(elenum)=8.0e8;
+                        elseif mat==4, % Polyprop
+                            ME(elenum)=3.45e8;
+                        elseif mat==5, % Polyethy
+                            ME(elenum)=6.9e8;
+                        elseif mat==6, % Kevlar
+                            ME(elenum)=6.9e10;
+                        elseif mat==7, % Aluminum
+                            ME(elenum)=7.6e10;
+                        elseif mat==8, % Dyneema
+                            ME(elenum)=1.0e11;
+                        end
+                    else
+                        H(4,elenum)=2; % flag for shackles and joiners
+                    endif
+                    Cd(elenum)=str2num(all_list(k,format(6,1):format(6,2)));
+                    elenum0=elenum;
+                    elenum=length(B)+1;
+                    insert=0;
+                    k=k+1;
+                elseif (k == rows(all_list) && match == 0)
+                    %printf("%d\n",)
+                    %printf("list(k+1): %s\nk: %d\nc(i,1): %d\n",list(k,:),k, c{i,1})
+                    printf("%d\n",match)
+                    %addelement_xls2;waitfor(h_push_save);
+                    addelement_xls2;
+                    set(h_edit_elename,'String',c{i,1});
+                    set(h_edit_elebuoy,'String',num2str(-1*c{i,3}));
+                    set(h_edit_eledim,'String',num2str(100*c{i,4}));
+                    add_name=get(h_edit_elename, 'String');
+                    add_buoy=get(h_edit_elebuoy, 'String');
+                    add_length=get(h_edit_eledim, 'String');
+                    warning("Please fill in data for the following: %s", c{i,1});
+                    %pause;
+                    k=1;
+                    match=0;
+                    %waitfor(h_push_add);
+                    save ('mdcodes.mat','acrels','cms','format','miscs','anchors','chains','floats','wires','all_list');
+                    all_list = [""];
+                    for m = 1:rows(floats)
+                        all_list(rows(all_list)+1,:)=floats(m,:);
+                    endfor
+                    for m = 1:rows(wires)
+                        all_list(rows(all_list)+1,:)=wires(m,:);
+                    endfor
+                    for m = 1:rows(chains)
+                        all_list(rows(all_list)+1,:)=chains(m,:);
+                    endfor
+                    for m = 1:rows(acrels)
+                        all_list(rows(all_list)+1,:)=acrels(m,:);
+                    endfor
+                    for m = 1:rows(cms)
+                        all_list(rows(all_list)+1,:)=cms(m,:);
+                    endfor
+                    for m = 1:rows(anchors)
+                        all_list(rows(all_list)+1,:)=anchors(m,:);
+                    endfor
+                    for m = 1:rows(miscs)
+                        all_list(rows(all_list)+1,:)=miscs(m,:);
+                    endfor
+                    waitfor(h_edit_elename);
+                    %waitfor(hmaincls);figure(4);clf;close;
+                else
+                    k=k+1;
+                endif
+            endwhile
+        endif
     endfor
+
+    insert=elenum;
+    mb=length(B);
+    bump=[insert+1:mb+1];
+    moorele(bump,:)=moorele(elenum:mb,:);
+    B(bump)=B(elenum:mb);
+    H(:,bump)=H(:,elenum:mb);
+    Cd(bump)=Cd(elenum:mb);
+    ME(bump)=ME(elenum:mb);
+
+
+    moorele(elenum,:)=anchors(1,format(1,1):format(1,2));
+    B(elenum)=str2num(anchors(1,format(2,1):format(2,2)));
+    H(1,elenum)=str2num(anchors(1,format(3,1):format(3,2)))/100; % convert to metres pm
+    H(2,elenum)=str2num(anchors(1,format(4,1):format(4,2)))/100;
+    H(3,elenum)=str2num(anchors(1,format(5,1):format(5,2)))/100;
+    H(4,elenum)=0;
+    ME(elenum)=inf;  % by default set modulus of elasticity to infinity (no stretch)
+
+
     close(ldng);
 
 
